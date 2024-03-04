ad415a689d023fbe32ba8fb9dd1a617989e7624f
erianda <erianda@localhost>
2024-03-04 17:01:30 +0700

    [First Commit] Hello

---
+++ ProjectPertama/src/pertemuan1/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author erianda
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x= 34;
+        String nama = "Aldo";
+        System.out.println("Nama saya "+nama);
+        System.out.println("Umur saya "+x+" tahun");
+    }
+    
+}
