36e7a0b4353de70cc0f4a93ef2b683141fae7dbb
erianda <erianda@localhost>
2024-03-18 14:25:07 +0700

    Operator di Java

---
+++ ProjectPertama/src/pertemuan1/OperatorJava.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author erianda
+ */
+public class OperatorJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int anakAyam = 100;
+        
+        for(anakAyam=100;anakAyam>0;anakAyam--){
+            if(anakAyam==1){
+                System.out.println("Anak ayam turun " +anakAyam+" mati satu tinggal induknya");
+            } else {
+                System.out.println("Anak ayam turun " +anakAyam+" mati satu tinggal ");
+            }
+        }
+    }
+    
+}
